f4c10af319edde4ee5eb8d18319ec394c2a816fd
Pottabathini Vivekananda <vivekananda.99666@gmail.com>
2024-12-27 20:10:44 +0530

    added cors

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 require('dotenv').config();
 const express = require('express');
 const mailRoutes = require('./routes/mailRoutes');
+const cors = requie('cors')
 
 const app = express();
 const PORT = 3000;
 app.use(express.json()); 
+app.use(cors());
 
 app.use('/api', mailRoutes);
 
